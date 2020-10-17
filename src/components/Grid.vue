c4174ff21d7e33bbcd90c04bdd10486f8282cc1d
masayado <masayado@uc.cl>
2020-10-17 02:53:00 -0300

    agregue componentes navbar y grid

---
+++ src/components/Grid.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="grid">
+    <b-container class="bv-example-row d-flex justify-content-center">
+        <b-row>
+            <b-col md="4" v-for="character in characters" :key="character.id" class="d-flex justify-content-center">
+                <b-card
+                    :title="character.name"
+                    :img-src="character.src"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-4"
+                    >
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'grid-component',
+    props: {
+        characterName:{
+            type:String,
+            required:true,
+        },
+        srcImg:{
+            type:String,
+            required:true,
+        },
+        id:{
+            type:Number,
+            required:true,
+        }
+    },
+    data: function(){
+        return {
+            characters:[
+                {id:1, name:'Morty', src:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'},
+                {id:2, name:'Rick',  src: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+                {id:3, name: 'Summer', src: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'},
+                {id:4, name: 'Beth', src: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'},
+                {id:5, name: 'Jerry', src: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'},
+            ]
+        }
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    //components: {},
+}
+</script>
+
+<style scoped>
+
+.grid{
+    margin-top:50px;
+}
+
+</style>
